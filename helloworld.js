67745e028d505d79e37fd319deb38c8b9e2dfef6
huanghuiliang <1213733169@qq.com>
2015-07-12 22:12:18 +0800

    Test

---
+++ helloworld.js
@@ -0,0 +1,12 @@
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
+assert.throws(
+  function() {
+    throw new Error("Wrong value");
+  },
+  Error
+);
+console.log('Server running at http://127.0.0.1:1337/');
